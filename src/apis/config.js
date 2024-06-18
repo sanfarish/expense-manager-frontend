47ea2f3ec27a7d84bac97410fd52bc626a93836c
sanfarish <farisfalah@gmail.com>
2024-06-18 20:09:56 +0700

    update api & readme

--- src/apis/config.js
+++ src/apis/config.js
@@ -3,6 +3,6 @@ const config = (token) => {
 };
 
 // const hostTarget = 'http://localhost:3500/api/v1';
-const hostTarget = 'https://api.expensemanager.biz.id/api/v1';
+const hostTarget = 'http://98.142.245.14:30503/api/v1';
 
 export { config, hostTarget };
